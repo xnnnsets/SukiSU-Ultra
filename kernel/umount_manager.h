5d749d450899117c9eba554d33fd5b18e57857c3
solihin929 <muhamadsolihin019501@gmail.com>
2025-11-24 20:46:58 +0700

    test 010

--- kernel/umount_manager.h
+++ kernel/umount_manager.h
@@ -5,8 +5,6 @@
 #include <linux/list.h>
 #include <linux/spinlock.h>
 
-#define MAX_PATHS 600
-
 struct cred;
 
 enum umount_entry_state {
@@ -58,7 +56,7 @@ int ksu_umount_manager_init(void);
 void ksu_umount_manager_exit(void);
 int ksu_umount_manager_add(const char *path, int flags, bool is_default);
 int ksu_umount_manager_remove(const char *path);
-void ksu_umount_manager_execute_all(const struct cred *cred, const char **exclude_paths, u32 exclude_count);
+void ksu_umount_manager_execute_all(const struct cred *cred);
 int ksu_umount_manager_get_entries(struct ksu_umount_entry_info __user *entries, u32 *count);
 int ksu_umount_manager_clear_custom(void);
 
